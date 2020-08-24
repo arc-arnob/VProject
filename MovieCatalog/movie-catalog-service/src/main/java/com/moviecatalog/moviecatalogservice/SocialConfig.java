a277d092303d3476cfe01d05752398c6888936c6
Arnob Chowdhury <as9025@srmist.edu.in>
2020-08-24 22:09:01 +0530

    Oauth Authentication implemented

--- MovieCatalog/movie-catalog-service/src/main/java/com/moviecatalog/moviecatalogservice/SocialConfig.java
+++ MovieCatalog/movie-catalog-service/src/main/java/com/moviecatalog/moviecatalogservice/SocialConfig.java
@@ -1,33 +1,29 @@
 package com.moviecatalog.moviecatalogservice;
 
-import com.moviecatalog.moviecatalogservice.api.Resources.Facebook;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.web.context.annotation.RequestScope;
+
 
 @Configuration
-public class SocialConfig {
+public class SocialConfig extends WebSecurityConfigurerAdapter{
 
-    @Bean
-	@RequestScope
-	public Facebook facebook(OAuth2AuthorizedClientService clientService) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String accessToken = null;
-		if (authentication.getClass().isAssignableFrom(OAuth2AuthenticationToken.class)) {
-			OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-			String clientRegistrationId = oauthToken.getAuthorizedClientRegistrationId();
-			if (clientRegistrationId.equals("facebook")) {
-				OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(clientRegistrationId, oauthToken.getName());
-				accessToken = client.getAccessToken().getTokenValue();
-			}
-		}
-		return new Facebook(accessToken);
-	}
+	@Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        
+        http.authorizeRequests()
+         .anyRequest().authenticated()
+         .and()
+         .oauth2Login();
+    }
+
+   
     
 }
--- MovieCatalog/movie-catalog-service/src/main/java/com/moviecatalog/moviecatalogservice/controller/HomeController.java
+++ MovieCatalog/movie-catalog-service/src/main/java/com/moviecatalog/moviecatalogservice/controller/HomeController.java
@@ -1,17 +1,29 @@
 package com.moviecatalog.moviecatalogservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import com.moviecatalog.moviecatalogservice.api.Resources.Facebook;
 import com.moviecatalog.moviecatalogservice.model.*;
 
 @RestController
@@ -21,11 +33,35 @@ public class HomeController {
     @Autowired
     RestTemplate restTemplate;
 
-    Facebook facebook;
-    
+    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+
+  
     @Autowired
-    public HomeController(Facebook facebook){
-        this.facebook = facebook;
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+    @GetMapping("/loginSuccess")
+    public String getLoginInfo(Model model, OAuth2AuthenticationToken authentication) {
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
+
+        String userInfoEndpointUri = client.getClientRegistration()
+            .getProviderDetails()
+            .getUserInfoEndpoint()
+            .getUri();
+
+        if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+                .getTokenValue());
+
+            HttpEntity<String> entity = new HttpEntity<String>("", headers);
+
+            ResponseEntity<Map> response = restTemplate.exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+            Map userAttributes = response.getBody();
+            model.addAttribute("name", userAttributes.get("name"));
+        }
+
+        return "loginSuccess";
     }
 
 
