2e509e0f1a79787f95e9beef6a47deeeffb38fd6
Arnob Chowdhury <as9025@srmist.edu.in>
2020-08-25 17:17:42 +0530

    removed oAuth- added eureka

--- Support/Eureka-Server/discovery-service/src/main/java/com/eurekaserver/discoveryservice/DiscoveryServiceApplication.java
+++ Support/Eureka-Server/discovery-service/src/main/java/com/eurekaserver/discoveryservice/DiscoveryServiceApplication.java
@@ -2,12 +2,11 @@ package com.eurekaserver.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableDiscoveryClient
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
