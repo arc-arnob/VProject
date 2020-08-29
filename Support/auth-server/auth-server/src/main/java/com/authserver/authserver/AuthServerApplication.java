3f85f4646e8ccffc1ebf3c722aed46c8f3f370d6
Arnob Chowdhury <as9025@srmist.edu.in>
2020-08-30 01:34:07 +0530

    configured zuul server for downstream

--- Support/auth-server/auth-server/src/main/java/com/authserver/authserver/AuthServerApplication.java
+++ Support/auth-server/auth-server/src/main/java/com/authserver/authserver/AuthServerApplication.java
@@ -2,9 +2,11 @@ package com.authserver.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableAuthorizationServer
 public class AuthServerApplication {
 
