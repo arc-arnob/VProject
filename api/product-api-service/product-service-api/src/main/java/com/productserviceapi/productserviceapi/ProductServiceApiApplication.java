aedd43896160d21a8efdd608ac14351cf600ca9b
Arnob Chowdhury <as9025@srmist.edu.in>
2020-09-01 11:11:32 +0530

    Removed ssl and added new paths in product-api

--- api/product-api-service/product-service-api/src/main/java/com/productserviceapi/productserviceapi/ProductServiceApiApplication.java
+++ api/product-api-service/product-service-api/src/main/java/com/productserviceapi/productserviceapi/ProductServiceApiApplication.java
@@ -24,39 +24,39 @@ public class ProductServiceApiApplication {
 		SpringApplication.run(ProductServiceApiApplication.class, args);
 	}
 
-	@Bean
-    public ServletWebServerFactory servletContainer() {
-        // Enable SSL Trafic
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
+	// @Bean
+    // public ServletWebServerFactory servletContainer() {
+    //     // Enable SSL Trafic
+    //     TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+    //         @Override
+    //         protected void postProcessContext(Context context) {
+    //             SecurityConstraint securityConstraint = new SecurityConstraint();
+    //             securityConstraint.setUserConstraint("CONFIDENTIAL");
+    //             SecurityCollection collection = new SecurityCollection();
+    //             collection.addPattern("/*");
+    //             securityConstraint.addCollection(collection);
+    //             context.addConstraint(securityConstraint);
+    //         }
+    //     };
 
-        // Add HTTP to HTTPS redirect
-        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+    //     // Add HTTP to HTTPS redirect
+    //     tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
 
-        return tomcat;
-    }
+    //     return tomcat;
+    // }
 
-    /*
-    We need to redirect from HTTP to HTTPS. Without SSL, this application used
-    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
-    redirected to HTTPS on 8443.
-     */
-    private Connector httpToHttpsRedirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
-    }
+    // /*
+    // We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    // port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    // redirected to HTTPS on 8443.
+    //  */
+    // private Connector httpToHttpsRedirectConnector() {
+    //     Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+    //     connector.setScheme("http");
+    //     connector.setPort(8080);
+    //     connector.setSecure(false);
+    //     connector.setRedirectPort(8443);
+    //     return connector;
+    // }
 
 }
