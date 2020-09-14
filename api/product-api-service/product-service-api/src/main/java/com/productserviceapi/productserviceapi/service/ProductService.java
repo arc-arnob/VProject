1d811878a0381b1b8af56ccfcd959718d15169b6
Arnob Chowdhury <as9025@srmist.edu.in>
2020-09-14 11:04:01 +0530

    added external api call

--- api/product-api-service/product-service-api/src/main/java/com/productserviceapi/productserviceapi/service/ProductService.java
+++ api/product-api-service/product-service-api/src/main/java/com/productserviceapi/productserviceapi/service/ProductService.java
@@ -4,8 +4,6 @@ import java.net.URI;
 import java.security.Principal;
 
 import com.productserviceapi.productserviceapi.model.Movie;
-import com.productserviceapi.productserviceapi.model.MovieList;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
